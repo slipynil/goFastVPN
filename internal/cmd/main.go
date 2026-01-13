41b487569fba47248827b97b263282c645b70a33
Vladimir Aristarkhov <slipynil@gmail.com>
2026-01-13 20:44:42 +0300

    upd

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -15,6 +15,9 @@ func main() {
 	defer WgClient.Close()
 
 	wgService := wireguard.WireGuardService("wg0", "127.0.0.1:5050", "5050", WgClient)
+	if err := wgService.ConfigureServer(); err != nil {
+		panic(err)
+	}
 	if err := wgService.DeviceInfo(); err != nil {
 		panic(err)
 	}
