a74dc92d19ec9c13dee0d9cd66d92f28ae88c7c8
Vladimir Aristarkhov <slipynil@gmail.com>
2026-01-15 22:50:11 +0300

    fix bugs

--- internal/feature/amneziawg/service/wireguard/show_peers.go
+++ internal/feature/amneziawg/service/wireguard/show_peers.go
@@ -14,7 +14,6 @@ func (s WireGuard) ShowPeers() error {
 
 	for _, peer := range device.Peers {
 		fmt.Println("---PEER CONNECTION---")
-		fmt.Println("Virtual Network:", peer.AllowedIPs[0])
 		fmt.Println("Public Key:", peer.PublicKey)
 		fmt.Println("Last Handshake:", peer.LastHandshakeTime)
 	}
