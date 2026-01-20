dc6ff6fc539563e0272534ec410a7623286c2232
Vladimir Aristarkhov <slipynil@gmail.com>
2026-01-20 09:49:03 +0300

    Switch AddPeer to id and enable config download
    - Use ID field in AddPeer request instead of file_name
    - Remove FilePath from AddPeerResponse
    - Post to /peers and return response body
    - DeletePeer now uses DELETE /peers/{id}
    - Add DownloadConfFile(publicKey) to fetch config
    - Update service to pass Chat.ID and upload config bytes

--- services/telegram/internal/dto/dto.go
+++ services/telegram/internal/dto/dto.go
@@ -7,11 +7,10 @@ type Message struct {
 
 type Request struct {
 	VirtualEndpoint string `json:"virtual_endpoint"`
-	FileName        string `json:"file_name"`
+	ID              int64  `json:"id"`
 }
 
 type AddPeerResponse struct {
 	Message   Message `json:"message"`
 	PublicKey string  `json:"public_key"`
-	FilePath  string  `json:"file_path"`
 }
