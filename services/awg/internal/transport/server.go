f89e61adfd9e0b87914999406c8fedeb6a7b3fd0
Vladimir Aristarkhov <slipynil@gmail.com>
2026-01-20 09:48:23 +0300

    Switch config endpoint to GET

--- services/awg/internal/transport/server.go
+++ services/awg/internal/transport/server.go
@@ -21,7 +21,7 @@ func (s *server) Start(endpoint string) {
 	r := mux.NewRouter()
 	r.HandleFunc("/peers/{id}", s.httpHandlers.DeletePeer).Methods("DELETE")
 	r.HandleFunc("/peers", s.httpHandlers.AddPeer).Methods("POST")
-	r.HandleFunc("/peers/{publicKey}/config", s.httpHandlers.SendConfFile).Methods("POST")
+	r.HandleFunc("/peers/{publicKey}/config", s.httpHandlers.SendConfFile).Methods("GET")
 
 	fmt.Printf("HTTP started on %s\n", endpoint)
 	http.ListenAndServe(endpoint, r)
