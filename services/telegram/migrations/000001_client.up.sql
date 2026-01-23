6fd1e0441cc04cec8e5e11a0a6cdfd434ea95907
Vladimir Aristarkhov <slipynil@gmail.com>
2026-01-23 10:00:37 +0300

    Add status column and index to client

--- services/telegram/migrations/000001_client.up.sql
+++ services/telegram/migrations/000001_client.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS client (
     host_id SERIAL PRIMARY KEY,
     telegram_username VARCHAR(255),
     telegram_id BIGINT,
+    status BOOLEAN DEFAULT TRUE,
     expires_at TIMESTAMP
 );
 
@@ -10,3 +11,4 @@ ALTER SEQUENCE client_host_id_seq RESTART WITH 2;
 
 CREATE INDEX IF NOT EXISTS idx_client_telegram_id ON client(telegram_id);
 CREATE INDEX IF NOT EXISTS idx_client_expires_at ON client(expires_at);
+CREATE INDEX IF NOT EXISTS idx_client_status ON client(status);
